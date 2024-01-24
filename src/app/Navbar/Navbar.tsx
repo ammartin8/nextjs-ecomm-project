6669c0105e1ebffcd0ebea7109baef69498cdfae
ammartin8 <ammartin8842@gmail.com>
2024-01-23 21:20:24 -0500

    Added cart page, footer,  and cart functionality

--- src/app/Navbar/Navbar.tsx
+++ src/app/Navbar/Navbar.tsx
@@ -1,19 +1,45 @@
 import Image from "next/image";
 import Link from "next/link";
-import logo from "@/assets/logo.png"
+import logo from "@/assets/logo.png";
+import { redirect } from "next/navigation";
+import { getCart } from "@/lib/db/cart";
+import ShoppingCartButton from "./ShoppingCartButton";
 
-export default function Navbar() {
-    return(
-        <div className="bg-base-100">
-            <div className="navbar max-w-7xl m-auto flex-col sm:flex-row gap-2">
-                <div className="flex-1">
-                    <Link href="/" className="btn btn-ghost text-xl normal-case">
-                        <Image src={logo} height={40} width={40} alt="EcommZone logo" />
-                        EcommZone
-                    </Link>
-                </div>
+async function searchProducts(formData: FormData) {
+  "use server";
 
-            </div>
+  const searchQuery = formData.get("searchQuery")?.toString();
+
+  if (searchQuery) {
+    redirect("/search?query=" + searchQuery);
+  }
+}
+
+export default async function Navbar() {
+    const cart = await getCart();
+
+  return (
+    <div className="bg-base-100">
+      <div className="navbar m-auto max-w-7xl flex-col gap-2 sm:flex-row">
+        <div className="flex-1">
+          <Link href="/" className="btn btn-ghost text-xl normal-case">
+            <Image src={logo} height={40} width={40} alt="EcommZone logo" />
+            EcommZone
+          </Link>
         </div>
-    )
+        <div className="flex-none gap-2">
+          <form action={searchProducts}>
+            <div className="form-control">
+              <input
+                name="searchQuery"
+                placeholder="Search"
+                className="min-w-200 input input-bordered w-full"
+              />
+            </div>
+          </form>
+          <ShoppingCartButton cart={cart} />
+        </div>
+      </div>
+    </div>
+  );
 }
